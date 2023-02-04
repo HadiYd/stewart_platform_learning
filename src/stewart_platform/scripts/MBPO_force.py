147455ee179e926ebc662f4a8f0dfdbf33f88425
Hadi Yd <yada.hadi@gmail.com>
2023-02-04 23:26:11 +0300

    added MBPO algorithm

--- src/stewart_platform/scripts/MBPO_force.py
+++ src/stewart_platform/scripts/MBPO_force.py
@@ -367,5 +367,49 @@ def main():
         agent.train(max_episodes=700)
     
 
+def main(args=None):
+
+    rospy.init_node('stewart_gym_MBPO')
+    env_name = 'StewartPose-v0'
+    if args is None:
+        args = readParser()
+
+    # Initial environment
+    env = gym.make(env_name)
+
+
+
+    # Intial agent
+    agent = Agent(env)
+
+    # Initial ensemble model
+    state_size = np.prod(env.observation_space.shape)
+    action_size = np.prod(env.action_space.shape)
+
+    env_model = construct_model(obs_dim=state_size, act_dim=action_size, hidden_dim=args.pred_hidden_size, num_networks=args.num_networks,
+                                    num_elites=args.num_elites)
+
+    # Predict environments
+    predict_env = PredictEnv(env_model, args.env_name, args.model_type)
+
+    # Initial pool for env
+    env_pool = ReplayMemory(args.replay_size)
+    # Initial pool for model
+    rollouts_per_epoch = args.rollout_batch_size * args.epoch_length / args.model_train_freq
+    model_steps_per_epoch = int(1 * rollouts_per_epoch)
+    new_pool_size = args.model_retain_epochs * model_steps_per_epoch
+    model_pool = ReplayMemory(new_pool_size)
+
+    # Sampler of environment
+    env_sampler = EnvSampler(env, max_path_length=args.max_path_length)
+
+    # Train or play the trained one!
+    project_name = "MBPO_force"
+
+    print("training")
+    wandb.init(name=f'MBPO_run_{args.run}',project=f"Train_and_Save_{project_name}")
+    train(args, env_sampler, predict_env, agent, env_pool, model_pool)
+
+
 if __name__ == "__main__":
     main()
