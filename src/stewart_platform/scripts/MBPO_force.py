a189aa5ec727bb452e3f682fcaa18b273a034903
Hadi Yd <yada.hadi@gmail.com>
2023-03-24 19:09:56 +0300

    moved args to args.py

--- src/stewart_platform/scripts/MBPO_force.py
+++ src/stewart_platform/scripts/MBPO_force.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
-# import tensorflow as tf
-# import tensorflow.keras as keras
-# from tensorflow.keras.layers import Input, Dense, Lambda, concatenate
-
 import gym
-import argparse
 import numpy as np
 # import random
 from collections import deque
@@ -21,108 +16,10 @@ from MBPO.sample_env import EnvSampler
 from MBPO.tf_models.constructor import construct_model  #, format_samples_for_training
 from MBPO.MBPO_main import train , play_trained
 from MBPO.tf_models.sac import SAC
-
-
-
-def readParser():
-    parser = argparse.ArgumentParser(description='MBPO')
-    ## For MBPO
-
-    parser.add_argument('--env_name', default="tewartPose-v0",
-                        help='Mujoco Gym environment (default: Hopper-v2)')
-    parser.add_argument('--seed', type=int, default=123456, metavar='N',
-                        help='random seed (default: 123456)')
-
-    parser.add_argument('--use_decay', type=bool, default=True, metavar='G',
-                        help='discount factor for reward (default: 0.99)')
-
-
-    # hyper paremters of SAC agent : 
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                        help='discount factor for reward (default: 0.99)')
-    parser.add_argument('--tau', type=float, default=0.005, metavar='G',
-                        help='target smoothing coefficient(τ) (default: 0.005)')
-    parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
-                        help='Temperature parameter α determines the relative importance of the entropy\
-                            term against the reward (default: 0.2)')
-    parser.add_argument('--policy', default="Gaussian",
-                        help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
-    parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
-                        help='Value target update per no. of updates per step (default: 1)')
-    parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
-                        help='Automaically adjust α (default: False)')
-    parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
-                        help='hidden size (default: 256)')
-    parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
-                        help='learning rate (default: 0.0003)')
-    
-
-    # hper parameters of ensembel model 
-
-    parser.add_argument('--num_networks', type=int, default=7, metavar='E',
-                        help='ensemble size (default: 7)')
-    parser.add_argument('--num_elites', type=int, default=5, metavar='E',
-                        help='elite size (default: 5)')
-    parser.add_argument('--pred_hidden_size', type=int, default=200, metavar='E',
-                        help='hidden size for predictive model')
-    
-
-    parser.add_argument('--reward_size', type=int, default=1, metavar='E',
-                        help='environment reward size')
-
-    parser.add_argument('--replay_size', type=int, default=1000, metavar='N',
-                        help='size of replay buffer (default: 10000000)')
-
-    parser.add_argument('--model_retain_epochs', type=int, default=1, metavar='A',
-                        help='retain epochs')
-    parser.add_argument('--model_train_freq', type=int, default=2500, metavar='A',  #250
-                        help='frequency of training')
-    parser.add_argument('--rollout_batch_size', type=int, default=1000, metavar='A',
-                        help='rollout number M')
-    parser.add_argument('--epoch_length', type=int, default=200, metavar='A',
-                        help='steps per epoch')
-    parser.add_argument('--rollout_min_epoch', type=int, default=20, metavar='A',
-                        help='rollout min epoch')
-    parser.add_argument('--rollout_max_epoch', type=int, default=150, metavar='A',
-                        help='rollout max epoch')
-    parser.add_argument('--rollout_min_length', type=int, default=1, metavar='A',
-                        help='rollout min length')
-    parser.add_argument('--rollout_max_length', type=int, default=15, metavar='A',
-                        help='rollout max length')
-    parser.add_argument('--num_epoch', type=int, default=200, metavar='A',
-                        help='total number of epochs')
-    parser.add_argument('--min_pool_size', type=int, default=100, metavar='A',
-                        help='minimum pool size')
-    parser.add_argument('--real_ratio', type=float, default=0.05, metavar='A',
-                        help='ratio of env samples / model samples')
-    parser.add_argument('--train_every_n_steps', type=int, default=1, metavar='A',
-                        help='frequency of training policy')
-    parser.add_argument('--num_train_repeat', type=int, default=20, metavar='A',
-                        help='times to training policy per step')
-    parser.add_argument('--max_train_repeat_per_step', type=int, default=5, metavar='A',
-                        help='max training times per step')
-    parser.add_argument('--policy_train_batch_size', type=int, default=256, metavar='A',
-                        help='batch size for training policy')
-    parser.add_argument('--init_exploration_steps', type=int, default=500, metavar='A',
-                        help='exploration steps initially')
-    parser.add_argument('--max_path_length', type=int, default=200, metavar='A',
-                        help='max length of path')
-
-
-    parser.add_argument('--model_type', default='tensorflow', metavar='A',
-                        help='predict model -- pytorch or tensorflow')
-
-    parser.add_argument('--cuda', default=True, action="store_true",
-                        help='run on CUDA (default: True)')
-    
-    # hper parameters of runs , training or running
-    parser.add_argument('--run', type=int, default=10)
-    parser.add_argument('--load_checkpoint', type=bool, default=False)
-    return parser.parse_args()
+from MBPO.args import readParser
 
 
 # tf.keras.backend.set_floatx('float64')
-    
 
 def main(args=None):
 
